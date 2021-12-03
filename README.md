a59c292c49d0f453f53ffd374f38d008180ac7c4
Will Coquillette <4646219+willquill@users.noreply.github.com>
2021-12-03 18:20:44 -0500

    complete working script with several apps available

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# bootstrap-macos.sh
-macOS bootstrap script
+# bootstrap-macos
+
+Bootstrap a macOS system
+
+# Find brew packages
+
+Check this page out: [https://formulae.brew.sh/formula/](https://formulae.brew.sh/formula/)
+
+Clone this repo and update the app lists in the shell script if you want to install something else.
+
+# Run
+
+Make it executable
+
+`chmod +x bootstrap-macos.sh`
+
+Run it
+
+`./bootstrap-macos.sh`
---
+++ bootstrap-macos.sh
@@ -0,0 +1,114 @@
+#!/bin/sh
+
+# Confirm function
+# https://mike632t.wordpress.com/2017/07/06/bash-yes-no-prompt/
+confirm() {
+#
+# syntax: confirm [<prompt>]
+#
+# Prompts the user to enter Yes or No and returns 0/1.
+#
+# This  program  is free software: you can redistribute it and/or modify  it
+# under the terms of the GNU General Public License as published by the Free
+# Software  Foundation, either version 3 of the License, or (at your option)
+# any later version.
+#
+# This  program  is  distributed  in the hope that it will  be  useful,  but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public  License
+# for more details.
+#
+# You  should  have received a copy of the GNU General Public License  along
+# with this program. If not, see <http://www.gnu.org/licenses/>
+#
+#  04 Jul 17   0.1   - Initial version - MEJT
+#
+  local _prompt _default _response
+ 
+  if [ "$1" ]; then _prompt="$1"; else _prompt="Are you sure"; fi
+  _prompt="$_prompt [y/n] ?"
+ 
+  # Loop forever until the user enters a valid response (Y/N or Yes/No).
+  while true; do
+    read -r -p "$_prompt " _response
+    case "$_response" in
+      [Yy][Ee][Ss]|[Yy]) # Yes or Y (case-insensitive).
+        return 0
+        ;;
+      [Nn][Oo]|[Nn])  # No or N.
+        return 1
+        ;;
+      *) # Anything else (including a blank) is invalid.
+        ;;
+    esac
+  done
+}
+
+# Hostname function
+hostname() {
+	# Ask user for new hostnme
+	read -p 'Enter new hostname for computer: ' hostnamevar
+	echo
+
+	# Renames the 3 below 
+	/usr/sbin/scutil --set ComputerName $hostnamevar
+	/usr/sbin/scutil --set HostName $hostnamevar
+	/usr/sbin/scutil --set LocalHostName $hostnamevar
+
+	echo $hostnamevar is now the ComputerName, HostName, and LocalHostName
+}
+
+# Change hostname
+if confirm "Do you want to change hostname?"; then hostname; else echo Skipped change hostname; fi
+
+# Install brew
+if confirm "Do you want to install brew?":
+then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+  brew tap homebrew/cask
+  echo Brew successfully installed
+else echo You kinda need brew for this to work but okay; echo
+fi
+
+# Install Command Line Tools for Xcode
+if confirm "Do you want to install Command Line Tools for Xcode?"
+then xcode-select --install
+else echo Skipping Command Line Tools for Xcode; echo
+fi
+
+# Install apps
+apps=(vim)
+cask_apps=(archiver authy balenaetcher curl discord firefox git spotify steam sublime-text transmission vim virtualbox visual-studio-code vlc)
+
+# All apps
+if confirm "Do you want to install ALL of the following apps: ${apps[*]} ${cask_apps[*]}?"
+then
+  # Install all apps
+  for i in "${apps[@]}"
+  do
+    brew install $i && echo Successfully installed $i. && echo
+  done 
+  # Install all cask apps
+  for i in "${cask_apps[@]}"
+  do
+    brew install --cask $i && echo Successfully installed $i. && echo
+  done 
+else
+  echo
+  # Ask before each app
+  for i in "${apps[@]}"
+  do
+    if confirm "Do you want to install $i?"; then brew install $i
+    else echo Skipping $i && echo
+    fi
+  done
+  # Ask before each cask app 
+  for i in "${cask_apps[@]}"
+  do
+    if confirm "Do you want to install $i?"; then brew install --cask $i
+    else echo Skipping $i && echo
+    fi
+  done
+fi
+
+echo Script is complete
